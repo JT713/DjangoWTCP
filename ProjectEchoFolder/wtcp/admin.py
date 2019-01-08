268fb7a488ab482fde655dc2ce5dfc01294dc9ca
JT713 <jt05948n@pace.edu>
2019-01-08 15:34:30 -0500

    Hi Leanne here is the code

---
+++ ProjectEchoFolder/wtcp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+
+admin.site.Register(Student)
+admin.site.Register(nativeLanguage)
---
+++ ProjectEchoFolder/wtcp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WtcpConfig(AppConfig):
+    name = 'wtcp'
